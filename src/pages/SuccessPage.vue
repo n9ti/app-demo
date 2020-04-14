402a7cc62329bdad46075f553392d4a6020f798f
Nati <nati.n@linecorp.com>
2020-04-15 04:05:03 +0700

    add iframe

--- src/pages/SuccessPage.vue
+++ src/pages/SuccessPage.vue
@@ -1,5 +1,13 @@
 <template>
   <main>
+    <noscript
+      ><iframe
+        src="https://ga-landing.netlify.com/thank-indirect"
+        height="0"
+        width="0"
+        style="display:none;visibility:hidden"
+      ></iframe
+    ></noscript>
     <header>
       <h1>Success</h1>
       <h1>Thank you</h1>
